22cd3d265251b9836271dad4c0f706c581cef019
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-04-17 23:57:45 -0700

    factor out fetch dashboard and switch to dev_json_response for those hard-to-json-serialize objects

--- womp/dashboard.py
+++ womp/dashboard.py
@@ -6,7 +6,8 @@ import time
 from collections import defaultdict
 
 from wapiti import WapitiClient
-from clastic import Application, json_response
+from clastic import Application
+from clastic.render import dev_json_response
 from clastic.render.mako_templates import MakoRenderFactory
 from inputs import ALL_INPUTS
 
@@ -27,14 +28,14 @@ def article_list(list_manager):
     return {'article_lists': list_manager.get_list_dicts()}
 
 
-def input_server(input_name, title):
-    wc = WapitiClient('test@example.com')
-    if not title:
-        title = 'Coffee'
+def input_server(input_name, title=None, wapiti_client=None):
+    wc = wapiti_client or WapitiClient('test@example.com')
+    title = title or 'Coffee'
     page_info = wc.get_page_info(title)[0]
-    input_type = AVAIL_INPUTS.get(input_name.lower())
-    if input_type is None:
-        raise Exception('No input found with name "' + input_name + '"')
+    try:
+        input_type = AVAIL_INPUTS[input_name.lower()]
+    except KeyError:
+        raise ValueError('No input found with name %r' % (input_name,))
     get_input = input_type(page_info, wc)
     result = get_input()
     result['durations'] = get_input.durations
@@ -43,18 +44,23 @@ def input_server(input_name, title):
 
 def fetch_task_dashboard(fetch_manager):
     cur_time = time.time()
-    fetch_failures = defaultdict(list)
-    process_failures = defaultdict(list)
-    success_count = len([o for o in fetch_manager.result_stats if o['is_successful']])
+    fetch_failures = ff = defaultdict(list)
+    process_failures = pf = defaultdict(list)
+    success_count = len([o for o in fetch_manager.result_stats
+                         if o['is_successful']])
     failure_count = len(fetch_manager.result_stats) - success_count
-    in_prog_times = dict([(o.page_info.title, cur_time - o.times['create']) for o in fetch_manager.pool])
-    for input_name in fetch_manager.inputs:
-        fetch_failures[input_name.__name__].extend([f['title'] for f in fetch_manager.result_stats
-                                                    if not f['inputs'][input_name.__name__]['fetch_succeeded']])
+    in_prog_times = dict([(o.page_info.title, cur_time - o.times['create'])
+                          for o in fetch_manager.pool])
+    start_time = time.strftime("%d %b %Y %H:%M:%S UTC",
+                               time.gmtime(fetch_manager.start_time)),
+    for inp in fetch_manager.inputs:
+        inp_name = inp.__name__
+        ff[inp_name].extend([f['title'] for f in fetch_manager.result_stats
+                             if not f['inputs'][inp_name]['fetch_succeeded']])
         # todo: safe specific process errors; display in dashboard
-        process_failures[input_name.__name__].extend([f['title'] for f in fetch_manager.result_stats
-                                                     if f['inputs'][input_name.__name__]['fetch_succeeded'] and
-                                                     not f['inputs'][input_name.__name__]['is_successful']])
+        pf[inp_name].extend([f['title'] for f in fetch_manager.result_stats
+                             if f['inputs'][inp_name]['fetch_succeeded'] and
+                             not f['inputs'][inp_name]['is_successful']])
     ret = {'in_progress_count': len(fetch_manager.pool),
            'in_progress': in_prog_times,
            'complete_count': len(fetch_manager.results),
@@ -64,43 +70,44 @@ def fetch_task_dashboard(fetch_manager):
            'input_classes': [i.__name__ for i in fetch_manager.inputs],
            'in_progress': [o.get_status() for o in fetch_manager.pool],
            'complete_times': fetch_manager.result_stats,
-           'start_time': time.strftime("%d %b %Y %H:%M:%S UTC", time.gmtime(fetch_manager.start_time)),
+           'start_time': start_time,
            'duration': time.time() - fetch_manager.start_time,
            'fetch_failures': fetch_failures,
            'process_failures': process_failures,
-           'name': fetch_manager.name,
-           }
+           'name': fetch_manager.name}
     return ret
 
 
-def create_input_server():
+def create_input_server(wapiti_client=None):
+    wapiti_client = wapiti_client or WapitiClient('test@example.com')
+    resources = {'wapiti_client': wapiti_client}
     routes = [('/', input_list, 'list.html'),
-              ('/<input_name>/<title>', input_server, json_response)]
+              ('/<input_name>/<title>', input_server, dev_json_response)]
     mako_render = MakoRenderFactory(_TEMPLATE_PATH)
-    return Application(routes, render_factory=mako_render)
+    return Application(routes, resources, mako_render)
+
+
+def create_fetch_dashboard(fetch_manager):
+    routes = [('/', fetch_task_dashboard, 'dashboard.html'),
+              ('/json', fetch_task_dashboard, dev_json_response)]
+    resources = {'fetch_manager': fetch_manager}
+    mako_render = MakoRenderFactory(_TEMPLATE_PATH)
+    return Application(routes, resources, mako_render)
 
 
 def create_dashboard(womp_env):
+    wapiti_client = womp_env.get_wapiti_client()
+    input_server = create_input_server(wapiti_client)
+    fetch_dashboard = create_fetch_dashboard(womp_env.fetch_manager)
     resources = {'womp_env': womp_env,
-                 'fetch_manager': womp_env.fetch_manager,
                  'list_manager': womp_env.list_manager}
-    input_server = create_input_server()
-    routes = [('/', fetch_task_dashboard, 'dashboard.html'),
-              ('/dashboard', fetch_task_dashboard, 'dashboard.html'),
-              ('/dashboard/json', fetch_task_dashboard, json_response),
-              ('/article_list', article_list, 'article_list.html'),
-              ('/inputs', input_server)]
+    routes = [('/', fetch_dashboard),
+              ('/inputs', input_server),
+              ('/article_list', article_list, 'article_list.html')]
+
     mako_render = MakoRenderFactory(_TEMPLATE_PATH)
 
-    if not fetch_manager.dashboard_port:
-        fetch_manager.dashboard_port = 5000
-
     return Application(routes, resources, mako_render)
-    #app.serve(use_reloader=False,
-    #          static_prefix='static',
-    #          port=fetch_manager.dashboard_port,
-    #          static_path=static_path)
-
 
 
 if __name__ == '__main__':
