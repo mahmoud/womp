710dbcbfb9b1858e70b246e6242a6958b28b3de5
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-05-11 15:47:27 -0700

    make a proper setup.py and structure requirements.txt with dev packages at the top

--- setup.py
+++ setup.py
@@ -1,25 +1,52 @@
-# -*- coding: utf-8 -*-
+"""
+    womp
+    ~~~~
+
+    womp (Wikipedia Open Metrics Platform) is a workflow-oriented
+    suite, used for the collection, extraction, processing, and
+    management of data from Wikipedia APIs.
+
+    :copyright: (c) 2013 by Mahmoud Hashemi and Stephen LaPorte
+    :license: GPL, see LICENSE for more details.
+
+"""
+
+import sys
 from setuptools import setup
 
-setup(
-    name='womp',
-    description='Wikipedia Open Metrics Platform: data collection, extraction, and management',
-    version='0.0.1dev',
-    license='BSD',
-    author='Mahmoud Hashemi and Stephen LaPorte',
-    author_email='mahmoudrhashemi@gmail.com',
-    url='https://github.com/mahmoud/womp',
-    long_description=__doc__,
-    classifiers=(
-        #'Development Status :: 4 - Beta',
-        #'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 3',
-        #'Topic :: Internet :: WWW/HTTP',
-        #'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        #'Topic :: Text Processing :: Markup',
-    ),
-    py_modules=('womp',),
-    platforms='any'
-)
+
+__author__ = 'Mahmoud Hashemi and Stephen LaPorte'
+__version__ = '0.0.1'
+__contact__ = 'mahmoudrhashemi@gmail.com'
+__url__ = 'https://github.com/mahmoud/womp'
+__license__ = 'GPL'
+
+desc = ('Wikipedia Open Metrics Platform: data collection, '
+        'extraction, and management.')
+
+if sys.version_info < (2, 6):
+    raise NotImplementedError("Sorry, womp only supports Python >2.6")
+
+if sys.version_info >= (3,):
+    raise NotImplementedError("womp Python 3 support still en route")
+
+
+setup(name='womp',
+      version=__version__,
+      description=desc,
+      long_description=__doc__,
+      author=__author__,
+      author_email=__contact__,
+      url=__url__,
+      packages=['womp',
+                'womp.inputs'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['clastic',
+                        'pyquery',
+                        'lxml>=3.1',
+                        'Mako',
+                        'psutil'],
+      license=__license__,
+      platforms='any',
+      classifiers=['Programming Language :: Python :: 2.7'])
