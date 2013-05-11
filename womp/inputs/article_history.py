10856a454cda5f1567ea0fc8680e26f74483b45c
Stephen LaPorte <stephen.laporte@gmail.com>
2013-05-11 08:44:07 +0000

    in progress: ah using wapiti parsing

--- womp/inputs/article_history.py
+++ womp/inputs/article_history.py
@@ -137,10 +137,13 @@ class ArticleHistory(Input):
     prefix = 'ah'
 
     def fetch(self):
-        talk_content = self.wapiti.get_current_talk_content(self.info)
-        ret = talk_content[0].content
-        return ret
-
+        content = self.wapiti.get_current_talk_content(self.info)
+        parsed = self.wapiti.get_parsed_templates(content[0])[0]
+        1 / 0
+        return [template for template in parsed
+                if template.name.lower() == 'articlehistory'
+                or template.name.lower() == 'article history']
+    '''
     def process(self, f_res):
         ah_text = find_article_history(f_res)
         assessment_list = find_rating(f_res, rating_type='assessment') or []
@@ -177,6 +180,7 @@ class ArticleHistory(Input):
                   'importance_list': importance_list,
                 })
 
+
     stats = {
         'article_history': lambda f: len(f.get('actions')),
         'oldest_action_age': lambda f: f.get('oldest_action_age', 0),
@@ -193,4 +197,8 @@ class ArticleHistory(Input):
         'importance_average': lambda f: f['importance'],
         'importance_count': lambda f: f['importance_count'],
         'importance_list': lambda f: f['importance_list']
+    }
+    '''
+    stats = {
+        'article_history': lambda f: f,
     }
