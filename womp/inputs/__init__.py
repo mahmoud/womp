6e0c24f61843c6317bce3f1c54bf4780467f6b16
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-05-16 23:26:46 -0700

    restore accuracy to the name of ALL_INPUTS in inputs/__init__.py

--- womp/inputs/__init__.py
+++ womp/inputs/__init__.py
@@ -1,18 +1,43 @@
-from backlinks import Backlinks
-from grokse import PageViews
-from protection import Protection
+#from feedback import FeedbackV4  # removed from WP API
+#from feedback import FeedbackV5  # removed from WP API
 from article_history import ArticleHistory
-from langlinks import LangLinks
-from interwikilinks import InterWikiLinks
-from templates import ParsedTemplates
+from assessment import Assessment
+from backlinks import Backlinks
 from dom import DOM
+from google import GoogleNews
+from google import GoogleSearch
+from grokse import PageViews
+from interwikilinks import InterWikiLinks
+from langlinks import LangLinks
 from nineteen_dom import NineteenDOM
-from watchers import Watchers
+from protection import Protection
 from revisions import Revisions
+from templates import ParsedTemplates
+from watchers import Watchers
+from wikitrust import Wikitrust
 
 
-# BIG TODO: restore this to be all inputs again
-ALL_INPUTS = [Backlinks, PageViews, Protection, ArticleHistory, LangLinks, InterWikiLinks, ParsedTemplates, NineteenDOM]
+ALL_INPUTS = [ArticleHistory,
+              Assessment,
+              Backlinks,
+              DOM,
+              GoogleNews,
+              GoogleSearch,
+              InterWikiLinks,
+              LangLinks,
+              NineteenDOM,
+              PageViews,
+              ParsedTemplates,
+              Protection,
+              Revisions,
+              Watchers,
+              Wikitrust]
 
-
-DEFAULT_INPUTS = ALL_INPUTS
+DEFAULT_INPUTS = [ArticleHistory,
+                  Backlinks,
+                  InterWikiLinks,
+                  LangLinks,
+                  NineteenDOM,
+                  PageViews,
+                  ParsedTemplates,
+                  Protection]
